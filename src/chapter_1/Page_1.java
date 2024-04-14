14665dd156b49b4be35b87db4fc01e71b0dd80e4
product077 <products0707@gmail.com>
2024-04-14 10:57:48 -0400

    Added new print statement

--- src/chapter_1/Page_1.java
+++ src/chapter_1/Page_1.java
@@ -3,5 +3,6 @@ package chapter_1;
 public class Page_1 {
     public static void main(String[] args) {
         System.out.println("This is additional lines");
+        System.out.println("This is added from git_day2 branch");
     }
 }
